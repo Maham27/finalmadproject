31bd5ad63de1ef2372c4dc81c0d77090402978d1
Maham27 <mahamnaveed482@gmail.com>
2021-06-28 02:38:16 +0500

    init

--- app/src/main/java/com/example/ecommerceproject/informationactivity.java
+++ app/src/main/java/com/example/ecommerceproject/informationactivity.java
@@ -3,6 +3,7 @@ package com.example.ecommerceproject;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -88,6 +89,13 @@ public class informationactivity extends AppCompatActivity {
             }
 
 
+        });
+        buy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(informationactivity.this,address.class);
+                startActivity(intent);
+            }
         });
         imadd.setOnClickListener(new View.OnClickListener() {
             @Override
